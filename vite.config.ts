6de6809dc00ca0cb21af8681e1aad90e1f26c755
Henry Darnell <hjdarnel@users.noreply.github.com>
2022-02-22 16:44:44 -0600

    Favicon

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true,
+    open: false,
     proxy: {
       '/api': {
         target: 'http://localhost:8885/.netlify/functions',
